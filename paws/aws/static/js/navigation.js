671a3452f92ee0be89d1721f6092c6ae5d8e7486
katka07 <katka07@yahoo.com>
2023-08-10 23:20:11 +0530

    Added UI - 10AUG2023

--- paws/aws/static/js/navigation.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    if (cameFromHome === true) {
-        sessionStorage.removeItem('cameFromHome');
-    } else {
-        window.location.href = '/'; // Change this URL to your home page URL
-    }
-});
